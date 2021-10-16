c4c5b22b5fafd586b7bd6fb942ea111557594c45
Rene <renebitter@gmail.com>
2021-10-16 15:19:17 -0300

    getStaticProps; MeetupDetail component;

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,13 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 const MeetupDetails = () => {
   return (
-    <>
-      <img
-        src='https://images.unsplash.com/photo-1633966849409-15619ac38fff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1106&q=80'
-        alt='meetupimage'
-      />
-      <h1>Meetup</h1>
-      <address>Lalalalala</address>
-      <p>Description</p>
-    </>
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1595867818082-083862f3d630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+      title='First Meetup'
+      address='Some Street 12, Munich'
+      description='This is the first meetup'
+    />
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const DUMMY_MEETUPS = [
     id: 'm1',
     title: 'First Meetup',
     image:
-      'https://images.unsplash.com/photo-1633966849409-15619ac38fff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1106&q=80',
+      'https://images.unsplash.com/photo-1595867818082-083862f3d630?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
     address: 'Lalaland',
     description: 'This is the first Meetup',
   },
@@ -13,14 +13,23 @@ const DUMMY_MEETUPS = [
     id: 'm2',
     title: 'Second Meetup',
     image:
-      'https://images.unsplash.com/photo-1633966849409-15619ac38fff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1106&q=80',
+      'https://images.unsplash.com/photo-1543059080-f9b1272213d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80',
     address: 'Lalaland',
     description: 'This is the second Meetup',
   },
 ];
 
-const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+export function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
