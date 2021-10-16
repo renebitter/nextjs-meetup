368627c9afd479055edcca25a8ddbc32397734e9
Rene <renebitter@gmail.com>
2021-10-16 19:34:29 -0300

    CSS global variables

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -13,15 +13,17 @@ function MeetupItem(props) {
   return (
     <li className={classes.item}>
       <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={showDetailsHandler}>Show Details</button>
+        <div className={classes.card}>
+          <div className={classes.image}>
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className={classes.content}>
+            <h3>{props.title}</h3>
+            <address>{props.address}</address>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={showDetailsHandler}>Show Details</button>
+          </div>
         </div>
       </Card>
     </li>
