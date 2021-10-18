2b818188feebb33225d9965616d5b9339bd3d532
Rene Bitter <renebitter@gmail.com>
2021-10-18 17:07:44 -0300

    Send POST req to API

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,13 +3,13 @@ import { MongoClient } from 'mongodb';
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
     // const { title, image, address, description } = data;
 
-    MongoClient.connect(process.env.MONGODB_URI);
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
